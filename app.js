8c857364f90e15f9edd5dd1b5cfd1ed899c5d651
Darin Gregg <daringregg801@gmail.com>
2023-09-26 17:29:28 -0600

    revised JS Function exercise

--- app.js
+++ app.js
@@ -2,24 +2,22 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+    function printOdds(count) {
+        if(count < 0) {
+            console.log("Please provide a positive count.");
+            return;
+        }
+        
+        for (let i = 0; i <= count; i++) {
+            if (i % 2 === 1) {
+                console.log(i);
+            }
+        }
+    }
 
-function printOdds(count) {
-    //1. Initialization
-    //2. Conditional
-    //3. Incrementor
-    //Absolute value of number
-    const posCount = Math.abs(count);
-    const sign = (count/posCount);
-    for ( let i = 1; i <= posCount; i++ ) {
-       if (i % 2 == 1){
-       console.log(sign*i);
-}
-    
-}
-}
+printOdds(-3);
+printOdds(10);
 
-//printOdds(10);
-//printOdds(-33);
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
